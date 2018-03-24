8f3d42e1a1176d20cf6b0721238282a6be31df13
Nick-Damico <nickalan82@icloud.com>
2018-03-24 14:48:37 -0400

    add spce directory, add WikipediaSearch.js for testing specs

---
+++ scripts/spec/WikipediaSpec.js
@@ -0,0 +1,33 @@
+
+describe('Wikipedia Viewer', function() {
+  let wikipediaViewer;
+
+  beforeEach(function(){
+    wikipediaViewer = new WikipediaViewer();
+  })
+
+
+  it('instance can be created', function() {
+
+    expect(wikipediaViewer).toBeDefined();
+  });
+
+  it("has property 'apiKeyRandom' on instantiation for fetching random article", function() {
+
+    expect(wikipediaViewer.apiUrlRandom).toBeDefined();
+    expect(wikipediaViewer.apiUrlRandom).toEqual(API_RANDOM_URL)
+  });
+
+  it("has property 'apiKeyQuery' on instantiation for fetching articles with query string", function() {
+
+    expect(wikipediaViewer.apiUrlQuery).toBeDefined();
+    expect(wikipediaViewer.apiUrlQuery).toEqual(API_QUERY_URL);
+  })
+
+  it("has property baseUrl for linking to article page", function() {
+
+    expect(wikipediaViewer.baseUrl).toBeDefined();
+    expect(wikipediaViewer.baseUrl).toEqual(WIKI_URL);
+  })
+
+});
