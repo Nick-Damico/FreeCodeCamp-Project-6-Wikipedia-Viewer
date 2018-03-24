c2a56ea09942a1263dd3629671e32f94cee1c450
Nick-Damico <nickalan82@icloud.com>
2018-03-24 16:25:55 -0400

    update const variable for API_QUERY_URL, used as base url for query searches

--- scripts/src/WikipediaViewer.js
+++ scripts/src/WikipediaViewer.js
@@ -1,6 +1,6 @@
 console.log('wikipedia.js connected');
 const API_RANDOM_URL = 'https://en.wikipedia.org/wiki/Special:Random';
-const API_QUERY_URL = 'https://en.wikipedia.org/w/api.php';
+const API_QUERY_URL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=';
 const WIKI_URL = 'https://en.wikipedia.org/wiki/Main_Page';
 
 function WikipediaViewer() {
