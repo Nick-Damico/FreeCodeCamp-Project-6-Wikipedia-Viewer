c258992d62bb03dbe67f077584f9cc0760cfe515
Nick-Damico <nickalan82@icloud.com>
2018-03-25 20:45:42 -0400

    define gulp task 'sass' compiles .scss files to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 const gulp = require('gulp');
+const g_sass = require('gulp-sass');
+
+
+gulp.task('sass', function() {
+  return gulp.src('./app/scss/**/*.scss')
+             .pipe(g_sass())
+             .pipe(gulp.dest('app/css'));
+});
 
 gulp.task('default', function() {
 
