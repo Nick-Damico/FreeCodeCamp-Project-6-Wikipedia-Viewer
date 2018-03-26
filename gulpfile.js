2b48ec5c4e7be9128aa81b4150631078172463b9
Nick-Damico <nickalan82@icloud.com>
2018-03-26 17:00:45 -0400

    add browser-sync npm package, watch task defined will live reload browser on change of js, scss, html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 const gulp = require('gulp');
 const g_sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    });
+});
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
              .pipe(g_sass())
-             .pipe(gulp.dest('app/css'));
+             .pipe(gulp.dest('app/css'))
+             .pipe(browserSync.reload({
+               stream: true
+             }))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['browserSync'], function() {
   gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
-
-// gulp.task('default', function() {
-//
-// });
