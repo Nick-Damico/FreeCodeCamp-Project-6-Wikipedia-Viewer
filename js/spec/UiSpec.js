2a6ad5f3f8b8388de11753c572682489ec46a3e1
Nick-Damico <nickalan82@icloud.com>
2018-03-27 16:24:59 -0400

    add Specs for testing Ui input and form elements

--- js/spec/UiSpec.js
+++ js/spec/UiSpec.js
@@ -1,7 +1,18 @@
+
 describe('Wikipedia App UI', function() {
+  let form;
+  beforeEach(function() {
+    form = document.getElementById('search');
+  });
 
   it('has an H1 element with class "main__title"', function() {
     const title = document.querySelector('.main__title');
     expect(title.textContent).toEqual("Wikipedia Viewer App");
   });
+
+  it('has an form element with id "main__form"', function () {
+
+    expect(form.getAttribute("id")).toEqual("search");
+  });
+
 });
