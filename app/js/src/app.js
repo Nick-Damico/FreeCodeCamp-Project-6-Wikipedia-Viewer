376207a4b077bfa5a14ae28e99e8de4ec22c5f0f
Nick-Damico <nickalan82@icloud.com>
2018-03-27 21:05:31 -0400

    define iife function to instantiate WikipediaViewer, add EventListener to search form on a 'submit' event, evokes WikipediaViewer.getArticles() method

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -1,1 +1,18 @@
-console.log('app.js connected');
+// iffe function call
+(function IIFE(){
+  // Instantiate Wikipedia Viewer Object.
+  const WikiViewer = new WikipediaViewer();
+  const searchForm = document.getElementById('search');
+  const searchInput = document.getElementById('search__input');
+  const articleContainer = document.getElementById('article_container');
+
+  searchForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const term = searchInput.value;
+    searchInput.value = '';
+    articleContainer.appendChild = '<span class="loading">Loading&amp;</span>';
+    WikiViewer.getArticles(term);
+  });
+
+
+})();
