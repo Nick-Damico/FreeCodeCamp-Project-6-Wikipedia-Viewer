625bfe7701e8c68634c82bdc507c1c138a4597d9
Nick-Damico <nickalan82@icloud.com>
2018-04-07 12:55:10 -0400

    add event handler for button 'click' of 'feeling lucky?'

--- js/src/app.js
+++ js/src/app.js
@@ -4,15 +4,24 @@
   const WikiViewer = new WikipediaViewer();
   const searchForm = document.getElementById('search');
   const searchInput = document.getElementById('search__input');
+  const searchBtnRandom = document.getElementById('search__btn--random');
   const articleContainer = document.getElementById('article_container');
 
   searchForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    const term = searchInput.value;
-    searchInput.value = '';
-    articleContainer.appendChild = '<span class="loading">Loading&amp;</span>';
-    WikiViewer.getArticles(term);
+    if (searchInput.value.length > 2) {
+      const term = searchInput.value;
+      searchInput.value = '';
+      articleContainer.appendChild = '<span class="loading">Loading&amp;</span>';
+      WikiViewer.getArticles(term);
+    }
   });
+
+  searchBtnRandom.addEventListener('click', function() {
+    WikiViewer.getRandom();
+  });
+
+
 
   function backToTopBtn() {
     const backTopBtn = document.querySelector('#backToTopbtn');
